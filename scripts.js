9a6a9cc7645211f99290458343467b23888bb1b0
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-01-31 18:40:32 +0000

    Create core functions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
 const choices = ["Fire", "Grass", "Water"];
+playerChoice = retrievePlayerChoice();
+console.log(playerChoice);
 
-let randomChoice = Math.floor(Math.random() * choices.length);
-let rivalChoice = (randomChoice, choices[randomChoice]);
-console.log(rivalChoice)
+rivalChoice = retrieveRivalChoice();
+console.log(rivalChoice);
+
+playRound = playRound();
+
+function retrievePlayerChoice() {
+    let playerChoice = prompt("Choice: ");
+    return(playerChoice);
+}
+
+function retrieveRivalChoice() {
+    let rivalChoice = Math.floor(Math.random() * choices.length);
+    return(rivalChoice, choices[rivalChoice]);
+}
+
+function playRound(playerChoice, rivalChoice) {
+    
+}
