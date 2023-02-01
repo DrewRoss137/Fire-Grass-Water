61cc71d3e7c42f8f3470bde21af15dfc54259899
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-01 19:19:13 +0000

    Update functions to get choices

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
+const buttons = document.querySelectorAll(".button");
 const choices = ["Fire", "Grass", "Water"];
 
 let rivalChoice = getRivalChoice();
 console.log("Rival Choice: " + rivalChoice);
 
-function getPlayerChoice() {
-    
-}
-
 function getRivalChoice() {
     let rivalChoice = choices[~~(Math.random() * choices.length)];
     return(rivalChoice);
 }
+
+buttons.forEach(button => {
+    button.addEventListener("click", function getPlayerChoice() {
+        const img = button.querySelector("img");
+        playerChoice = img.alt;
+        console.log(playerChoice)
+    })
+})
