04a44314ebbb69dd51c1ea7aa8349199c1ee0179
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-01 17:56:03 +0000

    Return button img alt text

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 const choices = ["Fire", "Grass", "Water"];
 const rivalChoice = choices[~~(Math.random() * choices.length)];
 console.log(rivalChoice);
+
+let buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const img = button.querySelector("img");
+        playerChoice = img.alt.toLowerCase();
+        console.log(playerChoice);
+    })
+})
