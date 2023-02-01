dcda8609be2cbfb708dc7f4aa58d17ba2595216f
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-01 18:04:31 +0000

    Create functions to get choices

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 const choices = ["Fire", "Grass", "Water"];
-const rivalChoice = choices[~~(Math.random() * choices.length)];
+const buttons = document.querySelectorAll(".button");
+
+let playerChoice;
+playerChoice = getPlayerChoice();
+let rivalChoice;
+rivalChoice = getRivalChoice();
 console.log(rivalChoice);
 
-let buttons = document.querySelectorAll(".button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
+function getPlayerChoice() {
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
         const img = button.querySelector("img");
-        playerChoice = img.alt.toLowerCase();
+        playerChoice = img.alt;
         console.log(playerChoice);
     })
 })
+}
+
+function getRivalChoice() {
+    const rivalChoice = choices[~~(Math.random() * choices.length)];
+    return(rivalChoice);
+}
+
+
+
+
+
