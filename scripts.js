9e37e50f183d827142f7ce5406ef443e2afc7019
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-01 18:42:31 +0000

    Create getRivalChoiceFunction

--- scripts.js
+++ scripts.js
@@ -1,28 +1,13 @@
 const choices = ["Fire", "Grass", "Water"];
-const buttons = document.querySelectorAll(".button");
 
-let playerChoice;
-playerChoice = getPlayerChoice();
-let rivalChoice;
-rivalChoice = getRivalChoice();
-console.log(rivalChoice);
+let rivalChoice = getRivalChoice();
+console.log("Rival Choice: " + rivalChoice);
 
 function getPlayerChoice() {
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-        const img = button.querySelector("img");
-        playerChoice = img.alt;
-        console.log(playerChoice);
-    })
-})
+    
 }
 
 function getRivalChoice() {
-    const rivalChoice = choices[~~(Math.random() * choices.length)];
+    let rivalChoice = choices[~~(Math.random() * choices.length)];
     return(rivalChoice);
 }
-
-
-
-
-
