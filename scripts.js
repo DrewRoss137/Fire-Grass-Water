0c9750322ef52a36ff25141b9bee2048d6db1713
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-03 20:11:20 +0000

    Format code

--- scripts.js
+++ scripts.js
@@ -3,18 +3,7 @@ const choices = ["Fire", "Grass", "Water"];
 let rivalChoice = getRivalChoice();
 console.log(rivalChoice);
 
-let imgAlt = getImgAlt();
-
 function getRivalChoice() {
     let randomRivalChoice = Math.floor(Math.random() * choices.length);
     return choices[randomRivalChoice];
-  }
-
-function getImgAlt() {
-    document.querySelectorAll("img").forEach(image => {
-      image.addEventListener("click", () => {
-        console.log(image.alt);
-        return(image.alt)
-      });
-    });
   }
