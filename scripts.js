154f413374d935d09e417d5cdc6d08bfc84ac564
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-03 16:46:23 +0000

    Clean getRivalChoice function code

--- scripts.js
+++ scripts.js
@@ -1,37 +1,8 @@
-const buttons = document.querySelectorAll(".button");
 const choices = ["Fire", "Grass", "Water"];
-
-let 
-let playerName;
-let playerScore = 0;
-
-let rivalName;
-let rivalScore = 0;
-
 let rivalChoice = getRivalChoice();
-console.log("Rival Choice: " + rivalChoice);
+console.log(rivalChoice);
 
 function getRivalChoice() {
-    let rivalChoice = choices[~~(Math.random() * choices.length)];
-    return(rivalChoice);
-}
-
-buttons.forEach(button => {
-    button.addEventListener("click", function getPlayerChoice() {
-        const buttonImg = button.querySelector("img");
-        playerChoice = buttonImg.alt;
-        console.log(playerChoice)
-    })
-})
-
-function getPokémonAttack() {
-
-}
-
-function playRound(playerChoice, rivalChoice) {
-
-}
-
-function getPokémonName() {
-
+    let randomRivalChoice = choices[~~(Math.random() * choices.length)];
+    return(randomRivalChoice);
 }
