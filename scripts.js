4976c37af18f6641ad989c25b5744b0a5acc920d
DrewRoss137 <DrewRoss137@Gmail.Com>
2023-02-04 14:50:39 +0000

    Create function to return img alt upon being clicked

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 const choices = ["Fire", "Grass", "Water"];
 
 let rivalChoice = getRivalChoice();
-console.log(rivalChoice);
+console.log(`Rival Choice: ${rivalChoice}`);
 
 function getRivalChoice() {
     let randomRivalChoice = Math.floor(Math.random() * choices.length);
     return choices[randomRivalChoice];
+};
+
+const images = document.querySelector("img");
+images.addEventListener("click", function(element) {
+  if (element.target.tagName === 'IMG') {
+    let playerChoice = element.target.alt;
+    console.log(`Player Choice: ${playerChoice}`);
+    playRound(playerChoice, rivalChoice);
+    
   }
+});
